7025d00253954213009cf66268212398ad71736e
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2024-08-07 20:38:18 -0400

    setting the code

---
+++ dp/dp/LCS.java
@@ -0,0 +1,20 @@
+package dp;
+
+public class LCS {
+	
+	public static int lcs_length (String X, String Y) {
+		/*
+		 * fill in your code here
+		 * Note: return the length of LCS, instead of c and b
+		 */
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(LCS.lcs_length("ABCBDAB", "BDCABA"));
+		System.out.println(LCS.lcs_length("ACCGGTCGAGTGCGCGGAAGCCGGCCGAA", "GTCGTTCGGAATGCCGTTGCTCTGTAAA"));
+	}
+
+}
